163e7a5ac450f11c41e25acad6e660ad1021d3f2
pawelataman <pawel.atmn@gmail.com>
2024-06-08 15:14:35 +0200

    Work on list

--- client/apps/frontend/src/app/feature/converter/file-list-item/file-list-item.component.ts
+++ client/apps/frontend/src/app/feature/converter/file-list-item/file-list-item.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { FileSizePipe } from '../../../core/pipes/file-size.pipe';
 import { FileToUrlPipe } from '../../../core/pipes/file-to-url.pipe';
+import { ConvertToSelectComponent } from '../convert-to-select/convert-to-select.component';
 import { ConvertableFile } from '../converter.types';
 
 @Component({
   selector: 'app-file-list-item',
   standalone: true,
-  imports: [CommonModule, FileToUrlPipe],
+  imports: [CommonModule, FileToUrlPipe, ConvertToSelectComponent, FileSizePipe],
   templateUrl: './file-list-item.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FileListItemComponent {
   file = input.required<ConvertableFile>();
