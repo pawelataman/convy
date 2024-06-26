14c0d8fbfb13a1709c6eeb8a4a278ebf3220007a
pawelataman <pawel.atmn@gmail.com>
2024-06-26 08:51:36 +0200

    Playaround with streams

--- client/apps/backend/src/app/domain/converter/converter.controller.ts
+++ client/apps/backend/src/app/domain/converter/converter.controller.ts
@@ -1,7 +1,9 @@
 import { File, FileInterceptor } from '@nest-lab/fastify-multer';
 import { Body, Controller, OnModuleInit, Post, UploadedFile, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Client, ClientGrpc } from '@nestjs/microservices';
-import { firstValueFrom, of } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Readable } from 'stream';
+import { FileUploadRequest, FileUploadResponse } from '../../core/proto-types/converter_messages';
 import { CONVERTER_SERVICE_NAME, ConverterServiceClient } from '../../core/proto-types/converter_service';
 import { ConverterSvcOptions } from './converter-svc-options';
 import { ConversionRequestMetadata } from './dto/convert-metadata';
@@ -19,15 +21,31 @@ export class ConverterController implements OnModuleInit {
   @Post('convert')
   @UseInterceptors(FileInterceptor('file'))
   @UsePipes(new ValidationPipe({ transform: true }))
-  async convertImage(@UploadedFile() file: File, @Body() metadata: ConversionRequestMetadata) {
+  async convertImage(@UploadedFile() file: File, @Body() metadata: ConversionRequestMetadata): Promise<FileUploadResponse> {
     console.log(metadata);
-    return firstValueFrom(
-      this.converterService.upload(
-        of({
+
+    const fileStream = bufferToStream(file.buffer);
+
+    const fileStreamObservable = new Observable<FileUploadRequest>((observer) => {
+      fileStream.on('data', (chunk) => {
+        observer.next({
+          chunk: chunk,
           fileName: file.originalname,
-          chunk: new Uint8Array(file.buffer),
-        })
-      )
-    );
+        });
+      });
+
+      fileStream.once('end', () => observer.complete());
+    });
+    return firstValueFrom(this.converterService.upload(fileStreamObservable));
   }
+}
+
+function bufferToStream(buffer): Readable {
+  // Create a readable stream from the buffer
+  const stream = new Readable({ highWaterMark: 128 });
+  // Push the buffer to the stream
+  stream.push(buffer);
+  // Signal the end of the stream
+  stream.push(null);
+  return stream;
 }
